59491ee9706ebc10388f321bc2054d1506650b45
Colin McKinney <mckinnec@wabash.edu>
2023-10-17 09:41:37 -0400

    Added stuff on conditionals

---
+++ oct17/conditionals1.asm
@@ -0,0 +1,20 @@
+.data
+
+hellostr: .asciz "Hello, world!"
+
+
+.text
+
+.global main
+main:
+    stp x29, x30, [sp, #-16]!
+
+    loop:
+    ldr x0, =hellostr
+    bl puts
+    b loop
+
+    ldp x29, x30, [sp], #16
+    mov w0, #0
+    ret
+    
