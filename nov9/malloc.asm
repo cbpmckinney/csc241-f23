02e132060f0d722dcc59206cd7c418ef413199d8
Colin McKinney <mckinnec@wabash.edu>
2023-11-10 14:33:46 -0500

    Added malloc() example

---
+++ nov9/malloc.asm
@@ -0,0 +1,24 @@
+.data
+
+mystring: .asciz "The pointer is %p.\n"
+myval: .word 0xFFFFFFFF
+myptr: .dword 0x0
+
+.text
+
+.global main
+main:
+    stp x29, x30, [sp, #-16]!
+
+    //ldr x0, =myval
+    //ldr w0, [x0]
+    mov w0, #32  //ask for 32 bytes of memory
+    bl malloc
+
+    mov x1, x0
+    ldr x0, =mystring
+    bl printf
+
+    ldp x29, x30, [sp], #16
+    mov w0, #0
+    ret
