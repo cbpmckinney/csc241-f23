e9cde415a0905f43fa3928111788397f7e5d2c3c
Colin McKinney <mckinnec@wabash.edu>
2023-11-14 08:32:14 -0500

    Updated malloc example

--- nov9/malloc.asm
+++ nov9/malloc.asm
@@ -3,6 +3,8 @@
 mystring: .asciz "The pointer is %p.\n"
 myval: .word 0xFFFFFFFF
 myptr: .dword 0x0
+instr: .asciz "%[^\n]31s"
+prmpt: .asciz "Type a string, bro\n"
 
 .text
 
@@ -15,13 +17,25 @@ main:
     mov w0, #32  //ask for 32 bytes of memory
     bl malloc
 
-    str x0, [sp, #-16]! //store pointer for later
+    //store pointer for later
+    ldr x1, =myptr
+    str x0, [x1]
 
     mov x1, x0
     ldr x0, =mystring
     bl printf
 
-    ldr x0, [sp], #16
+    ldr x0, =prmpt
+    bl puts
+
+    ldr x0, =instr
+    ldr x2, =myptr
+    ldr x1, [x2]
+    bl scanf 
+
+
+    ldr x1, =myptr
+    ldr x0, [x1]
     bl free  // free allocated memory
 
     ldp x29, x30, [sp], #16
